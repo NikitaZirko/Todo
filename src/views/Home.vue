a0c047e7e8f5225dff26d46cd3fe39451924f2e4
Nikita Zirko <nik.message@gmail.com>
2020-04-26 15:49:37 +0200

    added base styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <font-awesome-icon icon="plus-circle" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <font-awesome-icon icon="plus-circle" size="5x" />
+    <TodoList v-for="item in 6" :key="item.message" msg="Добавьте вашу заметку" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TodoList
+  },
+  computed: {
+    /* films() {
+      return this.$store.getters["films/films"];
+    } */
   }
 };
 </script>
+
+<style lang="scss" scoped>
+ @import '../assets/styles/pages/home.scss';
+</style>
